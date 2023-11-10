95983bd40aef32cb31a77642178250bf827c6f6b
deepthi mekala <143035056+KKFJF@users.noreply.github.com>
2023-11-09 21:32:06 -0600

    commit made in widget.js

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,5 +1,5 @@
 var widget = require('../models/widgetSchema');
-// List of all Costumes
+// List of all widgets
 exports.widget_list = async function(req, res) {
     try{
         thewidgets = await widget.find();
@@ -10,18 +10,31 @@ exports.widget_list = async function(req, res) {
         res.send(`{"error": ${err}}`);
         } 
 };
-// for a specific Costume.
-exports.widget_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
-};
-// Handle Costume create on POST.
+// for a specific widget.
+//exports.widget_detail = function(req, res) {
+// res.send('NOT IMPLEMENTED: widget detail: ' + req.params.id);
+// };
+// Handle widget create on POST.
+
+
+// for a specific widget.
+exports.widget_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await widget.findById(req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
 exports.widget_create_post = async function(req, res) {
     console.log(req.body)
     let document = new widget();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"widget_type":"goat", "cost":12, "size":"large"}
     document.name = req.body.name;
     document.description = req.body.description;
     document.price = req.body.price;
@@ -34,13 +47,13 @@ exports.widget_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     } ;
 };
-// Handle Costume delete form on DELETE.
+// Handle widget delete form on DELETE.
 exports.widget_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
+res.send('NOT IMPLEMENTED: widget delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle widget update form on PUT.
 exports.widget_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
+res.send('NOT IMPLEMENTED: widget update PUT' + req.params.id);
 };
  
 // VIEWS
