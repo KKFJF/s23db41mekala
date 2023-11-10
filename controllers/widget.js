a445f189440bfab042c1497500d1a1bb3f57b04b
deepthi mekala <143035056+KKFJF@users.noreply.github.com>
2023-11-09 22:33:18 -0600

    commit made

--- controllers/widget.js
+++ controllers/widget.js
@@ -52,8 +52,29 @@ exports.widget_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: widget delete DELETE ' + req.params.id);
 };
 // Handle widget update form on PUT.
-exports.widget_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: widget update PUT' + req.params.id);
+//exports.widget_update_put = function(req, res) {
+//res.send('NOT IMPLEMENTED: widget update PUT' + req.params.id);
+//};
+
+//Handle widget update form on PUT.
+exports.widget_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await widget.findById( req.params.id)
+// Do updates of properties
+if(req.body.costume_type)
+toUpdate.name = req.body.name;
+if(req.body.price) toUpdate.price = req.body.price;
+if(req.body.description) toUpdate.description = req.body.description;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
  
 // VIEWS
