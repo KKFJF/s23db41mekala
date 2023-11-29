4452583bc7cf720913d6a8e759ba9df4b7c2389c
deepthi mekala <143035056+KKFJF@users.noreply.github.com>
2023-11-29 15:19:57 -0600

    added buttons

--- models/widgetSchema.js
+++ models/widgetSchema.js
@@ -1,8 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const widgetSchema = mongoose.Schema({
-name: String,
-price: Number,
-description: String
-})
-module.exports = mongoose.model("widget",
-widgetSchema)
+    name: {
+        type: String,
+        required: true // "required" instead of "require"
+    },
+    description: {
+        type: String,
+        required: true // "required" instead of "require"
+    },
+    price: {
+        type: Number,
+        min: 10,
+        max: 40.99,
+        required: true // "required" instead of "require"
+    }
+});
+module.exports = mongoose.model("Widget", widgetSchema); // Capitalize "Widget" for the model name
--- routes/widget.js
+++ routes/widget.js
@@ -2,6 +2,16 @@ var express = require('express');
 const widget_controlers= require('../controllers/widget');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+
 router.get('/', widget_controlers.widget_view_all_Page);
 /* GET home page. */
 router.get('/detail', widget_controlers.widget_view_one_Page);
@@ -11,7 +21,7 @@ router.get('/create', widget_controlers.widget_create_Page);
 
 
 /* GET create update page */
-router.get('/update', widget_controlers.widget_update_Page);
+router.get('/update',secured, widget_controlers.widget_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', widget_controlers.widget_delete_Page);
