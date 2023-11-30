42cd3613ed2a1e6d009c8a6f9206997c530c8c72
deepthi mekala <143035056+KKFJF@users.noreply.github.com>
2023-11-29 20:14:13 -0600

    mekala mekala

--- routes/widget.js
+++ routes/widget.js
@@ -17,14 +17,14 @@ router.get('/', widget_controlers.widget_view_all_Page);
 router.get('/detail', widget_controlers.widget_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', widget_controlers.widget_create_Page);
+router.get('/create', secured, widget_controlers.widget_create_Page);
 
 
 /* GET create update page */
 router.get('/update',secured, widget_controlers.widget_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', widget_controlers.widget_delete_Page);
+router.get('/delete', secured,widget_controlers.widget_delete_Page);
 
 
 
